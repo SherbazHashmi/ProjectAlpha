21198a71628ed1b7f8034a70c54fe09f768acbcd
Sherbaz <sherbaz.hashmi@gmail.com>
2017-12-09 15:25:09 +1100

    Fixed UI Placement of Points Texts and Icons

---
+++ Assets/CurrencyIconLocation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyIconLocation : MonoBehaviour {
+
+    [SerializeField] Text currencyText;
+    [SerializeField] Image icon;
+    [SerializeField] Camera cam;
+  
+
+
+	// Use this for initialization
+	void Start () {
+        
+        /// Icon Placement
+        Vector3 currencyTextPosition = currencyText.rectTransform.position;
+        currencyTextPosition.x = currencyTextPosition.x - 0.7f;
+        currencyTextPosition.y = currencyTextPosition.y + 0.025f;
+        icon.transform.position = currencyTextPosition;
+
+
+	}
+
+
+}
