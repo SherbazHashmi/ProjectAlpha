6e50978129f421e2564adb7c176ae9dbed3c9dd6
Sherbaz <sherbaz.hashmi@gmail.com>
2017-12-13 17:26:23 +1100

    Added PelletGunDefaultWeak Class

---
+++ Assets/Project Alpha/Common/Scripts/WeaponScripts/Pellet Gun/PelletGunDefaultWeak.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using MoreMountains.CorgiEngine;
+
+namespace MoreMountains.CorgiEngine
+{
+
+    public class PelletGunBaseConfiguration : ProjectileWeapon
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
