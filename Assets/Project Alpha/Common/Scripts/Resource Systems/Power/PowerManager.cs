ee90289dd5276a5f5b752e2eacdef175acca5a4c
Sherbaz <sherbaz.hashmi@gmail.com>
2017-12-18 09:00:32 +1100

    Fleshed out power manager, have a depletion rate system.

--- Assets/Project Alpha/Common/Scripts/Resource Systems/Power/PowerManager.cs
+++ Assets/Project Alpha/Common/Scripts/Resource Systems/Power/PowerManager.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
-using System.Collections;
-using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
 namespace MoreMountains.CorgiEngine
 {
 
 	/// <summary>
-    /// Added This Class Just In Case We Choose To Passively Modify Power in Some Way. (Remove Power as Discussed with Ten).
+    /// Added This Class Just In Case We Choose To Passively Modify Power With Depletion Rates Based on Charges. (Remove Power as Discussed with Ten).
 	/// </summary>
 
 	public class PowerManager : MonoBehaviour
     {
 
 		GameObject gameManagerObject;
-		GameManager gameManager;
+        GameManager gameManager;
+        protected int DepletionRate { get; set; }
+        Utilities utils;
 
-       
-        void Start()
+
+
+		void Start()
         {
+            /// Instantiating a New Utilities Module For Dealing With Seconds.
+            utils = new Utilities();
+
+            /// Setting Depletion Rate
+            /// (PowerPoints per Second)
+            DepletionRate = 0;
+
 			try
 			{
 				/// Finding Object Called GameManager 
@@ -43,6 +53,33 @@ namespace MoreMountains.CorgiEngine
 				Debug.Log("gameManager Object Returning Null");
 			}
 
-		}  
+		}
+
+        void Update()
+        {
+            /// Setting Depletion Rate Based On Number Of Charges
+
+            switch (gameManager.Charge) {
+                case 0 :
+                    DepletionRate = 0;
+                    break;
+
+                case 1 :
+                    DepletionRate = 1;
+                    break;
+                case 2 :
+                    DepletionRate = 2;
+                    break;
+                case 3:
+                    DepletionRate = 5;
+                    break;
+                    
+            }
+
+            if (gameManager.Paused == false && gameManager.EnergyActive == false && utils.isWholeNumber(Time.timeSinceLevelLoad))
+            {
+                MMEventManager.TriggerEvent(new PowerEvent(PowerEventType.Remove, DepletionRate));
+            }
+        }
     }
 }
