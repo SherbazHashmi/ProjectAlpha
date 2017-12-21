52533a337e23e61782859c57fa2415657cfecc5c
Sherbaz <sherbaz.hashmi@gmail.com>
2017-12-21 14:20:24 +1100

    Added extended IM Button Class with a method ot change the button id. Which should hopefully allow us to rebind abilities to different buttons.
    Merged
    
    
    Merge remote-tracking branch 'origin/master'

---
+++ Assets/Project Alpha/Menu/Keybindings/IMButtonExtended.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using MoreMountains.Tools;
+using UnityEngineInternal;
+using MoreMountains;
+
+namespace MoreMountains.CorgiEngine
+{
+    public class IMButtonExtended : MMInput.IMButton
+    {
+        
+        public IMButtonExtended(string playerID, string buttonID, ButtonDownMethodDelegate btnDown, ButtonPressedMethodDelegate btnPressed, ButtonUpMethodDelegate btnUp) : base(playerID, buttonID, btnDown, btnPressed, btnUp)
+        {
+           
+        }
+        
+        
+        
+
+
+        public void changeButtonID(string buttonID)
+        {
+            string playerID = getPlayerID(ButtonID);
+            ButtonID = playerID + "_" + buttonID;
+
+
+        }
+
+
+        public string getPlayerID(string ButtonID)
+        {
+            if (ButtonID.Equals("_"))
+            {
+                return "";
+            }
+            else
+            {
+                return ButtonID[0] + getPlayerID(ButtonID.Substring(1));
+
+            }
+        }
+    }
+}
