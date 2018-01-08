4898495df55745bebd9e3b8cca55a2d5d1ed7ce4
Kevern <pretoriuskevern@gmail.com>
2018-01-08 03:49:00 +0200

    Added 3 Save Scripts
    Added QuantumStartData.cs, SaveHandler.cs and SaveLoadManager.cs

---
+++ Assets/Project Alpha/Menu/Main Menu/Scripts/QuantumStartData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+
+public class QuantumStartData : MonoBehaviour  
+{ 
+	public InputField enterPlayerName; 
+
+	private float timePlayed = 0; 
+	private Scene playerCurrentScene; 
+
+	public string playerName = ""; 
+	public int curremtHitPoints; 
+
+	public float TimePlayed 
+	{ 
+		get { return timePlayed; } 
+		set { timePlayed = value; } 
+	} 
+
+	public Scene PlayerCurrentScene 
+	{ 
+		get  { return playerCurrentScene; } 
+		set { playerCurrentScene = value; } 
+	} 
+
+	void Start ()  
+	{   
+		playerName = "Enter Name";                    //Set Input text 
+		curremtHitPoints = 100;                      //Set Starting Health 
+	} 
+
+	void Update ()  
+	{   
+		UnityEngine.PlayerPrefs.SetString ("Name", playerName);      //Save dedicated Name to PlayerPrefs 
+		UnityEngine.PlayerPrefs.SetInt("Hit Points", curremtHitPoints); //Saves starting hitpoints 
+		timePlayed += Time.deltaTime;                  //Keeps track of time 
+	} 
+
+	void OnGUI() 
+	{ 
+		enterPlayerName.text = "";                    //Allowes text to be entered into inputfield 
+		playerName = enterPlayerName.ToString();            //Sets playerName to text entered in inputfield 
+	} 
+}
---
+++ Assets/Project Alpha/Menu/SaveLoadData/Scripts/SaveLoadData.cs
@@ -0,0 +1,30 @@
+﻿﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.UI; 
+
+public class SavePrefabHandler : MonoBehaviour  
+{ 
+	public Image levelImage; 
+	public Text savegameName; 
+	public Text currentPlayTime; 
+	public Text playerScene; 
+	public Text dateSaved; 
+
+	private SaveHandler saveHandler; 
+	private QuantumStartData quantumStartData; 
+
+	void Start() 
+	{ 
+		saveHandler = GetComponent<SaveHandler> ();                    //calling the saveHandler to populate data 
+		quantumStartData = GetComponent<QuantumStartData> ();              //calling quantumstartdata to populate data; 
+	} 
+
+	public void SavePrefab() 
+	{ 
+		levelImage = saveHandler.playerLevelImage;                    //setting the image to required level iamge 
+		savegameName.text = quantumStartData.playerName;                //setting save game name to player name 
+		currentPlayTime.text = quantumStartData.TimePlayed.ToString ();          //recording time played 
+
+	} 
+} 
