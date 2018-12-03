07f707fd3d2224d08a8c5311af61a27109010b1e
Enrico Buzô <chewbak.13@gmail.com>
2018-12-03 05:15:33 -0200

    First energy task

---
+++ Energy_Enrico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UnderwolfStudios.ProjectAlpha
+{
+    int Mod;
+    public class GameManager : EventListener<EnergyEvent>
+    {
+
+        public int Energy { get; private set; }
+
+        public void OnEvent(EnergyEvent energyEvent)
+        {
+            switch (energyEvent.EnergyEventType)
+            {
+                case EnergyEventType.Add:
+                    _energy += Mod;
+                    break;
+                case EnergyEventType.Remove:
+                    _energy -= Mod;
+                    break;
+                case EnergyEventType.RemoveAll
+                    _energy:
+                    0;
+                    break;
+            }
+        }
+    }
+}
+			
